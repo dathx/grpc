7375f453445be253de4cb178678b71d7fab24a17
dathx <hxuandat2001@gmail.com>
2025-03-10 21:02:25 +0700

    init project

---
+++ src/main/java/com/grpc/grpc/app/GreetClient.java
@@ -0,0 +1,51 @@
+package com.grpc.grpc.app;
+
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.greeting.GreeterGrpc;
+import com.greeting.ServerResponse;
+import com.greeting.ClientRequest;
+
+public class GreetClient {
+    private static final Logger logger = Logger.getLogger(GreetClient.class.getName());
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public GreetClient(Channel channel) {
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void makeGreeting(String greeting, String username) {
+        logger.info("Sending greeting to server: " + greeting + " for name: " + username);
+        ClientRequest request = ClientRequest.newBuilder().setName(username).setGreeting(greeting).build();
+        logger.info("Sending to server: " + request);
+        ServerResponse response;
+        try {
+            response = blockingStub.greet(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        logger.info("Got following from the server: " + response.getMessage());
+    }
+
+    public static void main(String[] args) throws Exception {
+        String greeting = "Chao";
+        String username = "Fuc";
+        String serverAddress = "localhost:50051";
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(serverAddress)
+                .usePlaintext()
+                .build();
+        try {
+            GreetClient client = new GreetClient(channel);
+            client.makeGreeting(greeting, username);
+        } finally {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+}
---
+++ src/main/java/com/grpc/grpc/app/GreetServer.java
@@ -0,0 +1,53 @@
+package com.grpc.grpc.app;
+
+import com.greeting.ClientRequest;
+import com.greeting.ServerResponse;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import com.greeting.GreeterGrpc;
+import com.greeting.ClientRequest;
+import com.greeting.ServerResponse;
+
+public class GreetServer {
+    private static final Logger logger = Logger.getLogger(GreetServer.class.getName());
+    private Server server;
+    private void start() throws IOException {
+        int port = 50051;
+        server = ServerBuilder.forPort(port).addService(new GreeterImpl()).build().start();
+
+        logger.info("Server started, listening on " + port);
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.err.println("Shutting down gRPC server");
+                try {
+                    server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+            }
+        });
+    }
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+        @Override
+        public void greet(ClientRequest req, StreamObserver<ServerResponse> responseObserver) {
+            logger.info("Got request from client: " + req);
+            ServerResponse reply = ServerResponse.newBuilder().setMessage(
+                    "Server says " + "\"" + req.getGreeting() + " " + req.getName() + "\""
+            ).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final GreetServer greetServer = new GreetServer();
+        greetServer.start();
+        greetServer.server.awaitTermination();
+    }
+}
+
